6c19f76a372f1e5470f39c6a4af8339948240be3
Arnaud Benard <arnaudbenard13@gmail.com>
2013-12-27 10:13:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YoutubeJukebox::Application.config.secret_key_base = '6078baa94b7160b5d781932a507e6ef5c7f182d2ef6922250a5f467fb4285f1333b1f2cc45f175ef1728adf666582685f03f2d92aea708c99974f67e53329fc9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YoutubeJukebox::Application.config.session_store :cookie_store, key: '_youtube_jukebox_session'
